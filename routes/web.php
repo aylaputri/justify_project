ea31eb24efea524f91327b50c1f4a252db88530c
aylaputri <aylaputri12@upi.edu>
2026-05-05 14:08:26 +0700

    tambah folder

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/landing', function () {
+    return view('landing');
 });
+
+Route::get('/katalog', function () {
+    return view('page.katalog');
+});
+?>
 
